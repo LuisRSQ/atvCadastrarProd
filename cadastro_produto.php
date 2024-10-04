341fc900b1303cc82ed4039ce9330d1e416c6cd3
isaipers <isaias.p.conceicao@ba.estudante.senai.br>
2024-10-04 20:59:41 -0300

    .

--- cadastro_produto.php
+++ cadastro_produto.php
@@ -1,19 +1,16 @@
 <?php
-// Configuração do banco de dados
-$servername = "localhost"; // Host do banco de dados
-$username = "root";        // Nome de usuário
-$password = "";            // Senha
-$dbname = "cadastrar_produtos"; // Nome do banco de dados
 
-// Criar conexão
+$servername = "localhost"; 
+$username = "root";        
+$password = "";            
+$dbname = "cadastrar_produtos"; 
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar conexão
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
 
-// Verificar se os dados foram enviados pelo formulário
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $codigo_produto = $_POST["codigo_produto"];
     $nome_produto = $_POST["nome_produto"];
@@ -21,7 +18,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $preco_venda = $_POST["preco_venda"];
     $fornecedor = $_POST["fornecedor"];
 
-    // Inserir os dados na tabela
     $sql = "INSERT INTO produtos (codigo_produto, nome_produto, preco_compra, preco_venda, fornecedor)
             VALUES ('$codigo_produto', '$nome_produto', '$preco_compra', '$preco_venda', '$fornecedor')";
 
@@ -32,6 +28,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
-// Fechar conexão
 $conn->close();
 ?>
